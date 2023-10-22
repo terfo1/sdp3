73b0e1520677193a0e43efdce2823515e70fbfa1
terfo1 <alisher.temirhan@gmail.com>
2023-10-22 15:39:03 +0600

    sdp3

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+interface Audio{
+    void startmusic();
+    void stopmusic();
+}
+class iPod{
+    void iPodplay(){
+        System.out.println("iPod started to play");
+    }
+    void iPodstop(){
+        System.out.println("iPod turned off");
+    }
+}
+class SonyPlayer{
+    void Sonyplay(){
+        System.out.println("Sony started to play");
+    }
+    void Sonystop(){
+        System.out.println("Sony turned off");
+    }
+}
+    class iPodAdapter implements Audio{
+    iPod iPod;
+    public iPodAdapter(iPod iPod){
+        this.iPod=iPod;
+    }
+    public void startmusic(){
+        iPod.iPodplay();
+    }
+    public void stopmusic(){
+        iPod.iPodstop();
+    }
+}
+class SonyAdapter implements Audio{
+    SonyPlayer SonyPlayer;
+    public SonyAdapter(SonyPlayer Sonyplayer){
+        this.SonyPlayer= SonyPlayer;
+    }
+    public void startmusic(){
+        SonyPlayer.Sonyplay();
+    }
+    public void stopmusic(){
+        SonyPlayer.Sonystop();
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Audio iPodad=new iPodAdapter(new iPod());
+        Audio Sonyad=new SonyAdapter(new SonyPlayer());
+        iPodad.startmusic();
+        Sonyad.stopmusic();
+    }
+}
